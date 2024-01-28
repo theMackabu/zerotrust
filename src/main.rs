864b8e8b58ac7a44374e1121407ee0f4bee34688
theMackabu <theMackabu@gmail.com>
2024-01-27 21:58:09 -0800

    rewrite proxy: handle all methods

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,100 @@
 mod config;
 mod file;
 
+use awc::Client;
 use config::structs::Config;
-use futures::{channel::mpsc::unbounded, sink::SinkExt, stream::StreamExt};
+use futures_util::StreamExt;
 use macros_rs::string;
-use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_subscriber::{filter::LevelFilter, prelude::*};
+use url::Url;
 
 use actix_web::{
-    guard::GuardContext,
-    http::{header, StatusCode},
-    web::{BytesMut, Data, Path, Payload},
-    App, Error, HttpRequest, HttpResponse, HttpResponseBuilder, HttpServer,
+    dev::PeerAddr,
+    error::ErrorInternalServerError,
+    guard,
+    http::StatusCode,
+    web::{self, Data, Payload},
+    App, Error, HttpRequest, HttpResponse, HttpServer,
 };
 
-fn convert_headers(current_headers: &header::HeaderMap) -> HeaderMap {
-    let mut headers = reqwest::header::HeaderMap::new();
-
-    for (key, value) in current_headers.iter() {
-        let name = HeaderName::from_bytes(key.as_str().as_bytes()).expect("Failed to convert header name");
-        let value = HeaderValue::from_bytes(value.as_bytes()).expect("Failed to convert header value");
-        headers.insert(name, value);
-    }
-
-    return headers;
-}
-
-fn transfer_headers(mut proxied_response: HttpResponseBuilder, current_headers: &HeaderMap) -> HttpResponseBuilder {
-    for (key, value) in current_headers.iter() {
-        proxied_response.append_header((key, value));
-    }
-
-    return proxied_response;
-}
-
-fn guard_websocket(ctx: &GuardContext<'_>) -> bool { ctx.head().headers().get("upgrade").map_or(false, |h| h == "websocket") }
-
-#[actix_web::get("{url:.*}")]
-async fn get_proxy(url: Path<String>, req: HttpRequest, config: Data<Config>) -> HttpResponse {
+async fn proxy(req: HttpRequest, payload: Payload, peer_addr: Option<PeerAddr>, config: Data<Config>) -> Result<HttpResponse, Error> {
     tracing::info!(method = string!(req.method()), "request '{}'", req.uri());
 
+    // add initial setup (no services found & how to config)
+
     if let Some(name) = req.headers().get("SelectService") {
-        let service = match name.as_bytes() {
-            b"cs27" => format!("http://localhost:9309/{url}"),
-            _ => return HttpResponse::build(StatusCode::OK).body("not found"),
+        let mut url = match name.as_bytes() {
+            b"cs27" => Url::parse("http://localhost:9309").unwrap(),
+            _ => return Ok(HttpResponse::build(StatusCode::NOT_FOUND).body("Service not found")),
         };
 
-        let client = reqwest::Client::new();
-        let request = client.get(&service).headers(convert_headers(req.headers()));
-        let response = request.send().await.unwrap();
+        url.set_path(req.uri().path());
+        url.set_query(req.uri().query());
 
-        let proxied_response = HttpResponse::build(StatusCode::OK);
-        transfer_headers(proxied_response, response.headers()).streaming(response.bytes_stream())
+        let client = Client::builder().disable_redirects().finish();
+        let forwarded_req = client.request_from(url.as_str(), req.head()).no_decompress();
+
+        let forwarded_req = match peer_addr {
+            Some(PeerAddr(addr)) => forwarded_req.insert_header(("x-forwarded-for", addr.ip().to_string())),
+            None => forwarded_req,
+        };
+
+        let res = forwarded_req.send_stream(payload).await.map_err(ErrorInternalServerError)?;
+        let mut client_resp = HttpResponse::build(res.status());
+
+        for (header_name, header_value) in res.headers().iter().filter(|(h, _)| *h != "connection") {
+            client_resp.insert_header((header_name.clone(), header_value.clone()));
+        }
+
+        Ok(client_resp.streaming(res))
     } else {
-        HttpResponse::build(StatusCode::OK).body("not found")
+        Ok(HttpResponse::build(StatusCode::NOT_FOUND).body("No service header"))
     }
 }
 
-#[actix_web::get("{url:.*}", guard = "guard_websocket")]
-async fn ws_proxy(url: Path<String>, req: HttpRequest, mut payload: Payload) -> HttpResponse {
-    tracing::info!(method = string!(req.method()), "websocket '{}'", req.uri());
-
+pub async fn proxy_ws(client_req: HttpRequest, client_stream: Payload) -> Result<HttpResponse, Box<dyn std::error::Error>> {
     if let Some(name) = req.headers().get("SelectService") {
-        let service = match name.as_bytes() {
-            b"cs27" => format!("ws://localhost:9309/{url}"),
-            _ => return HttpResponse::build(StatusCode::OK).body("not found"),
+        let mut url = match name.as_bytes() {
+            b"cs27" => Url::parse("http://localhost:9309").unwrap(),
+            _ => return Ok(HttpResponse::build(StatusCode::NOT_FOUND).body("Service not found")),
         };
+        url.set_path(client_req.uri().path());
+        url.set_query(client_req.uri().query());
 
-        let client = awc::Client::new();
-        let mut ws = client.ws(service);
+        let mut req = reqwest::ClientBuilder::new().build().unwrap().get(url);
+        for (key, value) in client_req.headers() {
+            req = req.header(key, value);
+        }
+        let target_response = req.send().await.unwrap();
 
-        for (key, value) in req.headers().iter() {
-            ws = ws.header(key, value);
+        let status = target_response.status().as_u16();
+        if status != 101 {
+            return Err(Box::new(std::io::Error::new(std::io::ErrorKind::ConnectionRefused, "Target did not reply with 101 upgrade")));
         }
 
-        let (res, socket) = ws.connect().await.unwrap();
+        let mut client_response = HttpResponse::SwitchingProtocols();
+        client_response.upgrade("websocket");
+        for (header, value) in target_response.headers() {
+            client_response.insert_header((header.to_owned(), value.to_owned()));
+        }
 
-        assert_eq!(res.status().as_u16(), 101);
-
-        let mut io = socket.into_parts().io;
-        let (mut tx, rx) = unbounded();
-        let mut buf = BytesMut::new();
+        let target_upgrade = target_response.upgrade().await?;
+        let (target_rx, mut target_tx) = tokio::io::split(target_upgrade);
 
         actix_web::rt::spawn(async move {
-            loop {
-                tokio::select! {
-                    res = payload.next() => {
-                        match res {
-                            None => return,
-                            Some(body) => {
-                                let body = body.unwrap();
-                                io.write_all(&body).await.unwrap();
-                            }
-                        }
-                    }
-
-                    res = io.read_buf(&mut buf) => {
-                        let size = res.unwrap();
-                        let bytes = buf.split_to(size).freeze();
-                        tx.send(Ok::<_, Error>(bytes)).await.unwrap();
-                    }
-                }
+            let mut client_stream = client_stream.map(|result| result.map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err)));
+            let mut client_read = tokio_util::io::StreamReader::new(&mut client_stream);
+            let result = tokio::io::copy(&mut client_read, &mut target_tx).await;
+            if let Err(err) = result {
+                println!("Error proxying websocket client bytes to target: {err}")
             }
         });
 
-        HttpResponse::SwitchingProtocols().streaming(rx)
+        let target_stream = tokio_util::io::ReaderStream::new(target_rx);
+        Ok(client_response.streaming(target_stream))
     } else {
-        HttpResponse::build(StatusCode::OK).body("not found")
+        Ok(HttpResponse::build(StatusCode::NOT_FOUND).body("No service header"))
     }
 }
 
@@ -117,7 +103,12 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "INFO");
 
     let config = config::read();
-    let app = || App::new().app_data(Data::new(config::read())).service(ws_proxy).service(get_proxy);
+    let app = || {
+        App::new()
+            .app_data(Data::new(config::read()))
+            .service(web::scope("{url:.*}").guard(guard::Header("upgrade", "websocket")).route("", web::to(proxy_ws)))
+            .default_service(web::to(proxy))
+    };
 
     let formatting_layer = BunyanFormattingLayer::new("server".into(), std::io::stdout)
         .skip_fields(vec!["file", "line"].into_iter())
