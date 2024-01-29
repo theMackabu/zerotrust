36113322c06b2825c42023919cc7beff8ae6f5e3
theMackabu <theMackabu@gmail.com>
2024-01-28 19:56:14 -0800

    update gitignore

--- src/pages/dist/_sp/assets/login.b8ae0071.js
+++
@@ -1,9 +0,0 @@
-import{r as _}from"./index.ed373d49.js";var i={exports:{}},n={};/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var u=_,a=Symbol.for("react.element"),m=Symbol.for("react.fragment"),x=Object.prototype.hasOwnProperty,y=u.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function l(t,r,p){var e,o={},s=null,f=null;p!==void 0&&(s=""+p),r.key!==void 0&&(s=""+r.key),r.ref!==void 0&&(f=r.ref);for(e in r)x.call(r,e)&&!c.hasOwnProperty(e)&&(o[e]=r[e]);if(t&&t.defaultProps)for(e in r=t.defaultProps,r)o[e]===void 0&&(o[e]=r[e]);return{$$typeof:a,type:t,key:s,ref:f,props:o,_owner:y.current}}n.Fragment=m;n.jsx=l;n.jsxs=l;i.exports=n;var d=i.exports;const E=()=>d.jsx(_.Fragment,{children:"login page here!"});export{E as default};
