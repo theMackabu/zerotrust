6f74c5fe744817bf3b37b32016dbe67d2e6c39b8
theMackabu <theMackabu@gmail.com>
2024-01-29 22:55:29 -0800

    update routes

--- src/auth.rs
+++ src/auth.rs
@@ -3,11 +3,14 @@ use crate::pages::{render, TeraState};
 use macros_rs::string;
 use once_cell::sync::OnceCell;
 use serde::Deserialize;
+use serde_json::json;
 use tera::Context;
 
 use actix_web::{
+    cookie::Cookie,
+    dev::ConnectionInfo,
     http::{header::ContentType, StatusCode},
-    web::{BytesMut, Data, Form},
+    web::{Data, Form},
     HttpRequest, HttpResponse,
 };
 
@@ -15,7 +18,7 @@ use actix_web::{
 struct Login {
     email: String,
     password: String,
-    remember: String,
+    remember: Option<String>,
 }
 
 #[actix_web::get("/_sp/login")]
@@ -31,15 +34,46 @@ pub async fn login(req: HttpRequest, config: Data<&OnceCell<Config>>, tera: Data
     };
 
     page.insert("service_name", name);
+    page.insert("logged_in", &json!(false));
+    page.insert("button_status", "enabled");
+    page.insert("email_placeholder", &"");
+    page.insert("remember_checked", &"");
+    page.insert("password_placeholder", &"");
+
     let payload = render("login", &tera.0, &mut page);
 
     HttpResponse::build(StatusCode::OK).content_type(ContentType::html()).body(payload)
 }
 
 #[actix_web::post("/_sp/login")]
-pub async fn form_handler(req: HttpRequest, config: Data<&OnceCell<Config>>, body: Form<Login>) -> HttpResponse {
+pub async fn form_handler(req: HttpRequest, conn: ConnectionInfo, tera: Data<TeraState>, body: Form<Login>) -> HttpResponse {
     tracing::info!(method = string!(req.method()), "login '{}'", req.uri());
-    println!("{:?}", body);
 
-    HttpResponse::build(StatusCode::OK).body("logged in, redirect now")
+    let email = body.email.clone();
+    let password = body.password.clone();
+    let remember = body.remember.clone();
+
+    let tera = tera.get_ref();
+    let mut page = Context::new();
+
+    let name = match req.headers().get("SelectService") {
+        Some(name) => name.to_str().unwrap_or("(none)"),
+        None => "(none)",
+    };
+
+    match remember.unwrap_or(string!("off")).as_str() {
+        "on" => page.insert("remember_checked", &"checked"),
+        _ => page.insert("remember_checked", &""),
+    }
+
+    page.insert("service_name", name);
+    page.insert("logged_in", &json!(true));
+    page.insert("button_status", "disabled");
+    page.insert("email_placeholder", &email);
+    page.insert("password_placeholder", &"•".repeat(password.len()));
+
+    let payload = render("login", &tera.0, &mut page);
+    let cookie = Cookie::build(email, password).domain(conn.host()).secure(true).http_only(true).finish();
+
+    HttpResponse::build(StatusCode::OK).cookie(cookie).content_type(ContentType::html()).body(payload)
 }
