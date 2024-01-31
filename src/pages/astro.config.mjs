57d6028f8c1d6e3827c38a69c15aa6e045a11bae
theMackabu <theMackabu@gmail.com>
2024-01-31 00:55:32 -0800

    update rollup config

--- src/pages/astro.config.mjs
+++ src/pages/astro.config.mjs
@@ -5,5 +5,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
 	build: { format: 'file', assets: 'assets_provider' },
-	integrations: [tailwind(), react(), replace({ '/assets_provider/': '/{{prefix}}/assets/' })]
+	integrations: [tailwind(), react(), replace({ '/assets_provider/': '/{{prefix}}/assets/' })],
+	vite: { build: { rollupOptions: { output: { assetFileNames: 'assets_provider/[hash].[ext]' } } } }
 });
